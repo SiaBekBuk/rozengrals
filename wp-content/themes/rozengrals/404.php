8df6dfec6a9334e45794123d34cf2f636e8c55c4
gloock <alexgloock@gmail.com>
2014-01-08 14:19:53 +0200

    галерея

--- wp-content/themes/rozengrals/404.php
+++ wp-content/themes/rozengrals/404.php
@@ -1,7 +1,60 @@
-<?php get_header(); ?>
+<html>
+    <head>
+        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/fonts/CyrillicGoth/stylesheet.css" type="text/css"
+              media="all"/>
 
-	<h2>Error 404 - Page Not Found</h2>
+        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/fonts/old_london/stylesheet.css" type="text/css"
+              media="all"/>
+        <style type="text/css">
+            body {
+                background: #630800;
+                color: #42110e;
+            }
 
-<?php get_sidebar(); ?>
+            div {
+                margin-top: 100px;
+                text-align: center;
+            }
 
-<?php get_footer(); ?>
+            h1{
+                font-family: 'cyrillicgothregular';
+                font-size:72px;
+            }
+
+            span{
+                font: 21px/1.4 "Times New Roman", sans-serif;
+                /*font: 18px/1.4 Georgia, Serif;*/
+            }
+
+            a{
+                color: #c0873d;
+                font-size: 18px;
+                font:18px 'cyrillicgothregular';
+            }
+
+            a:hover{
+                color: #c0873d;
+                text-decoration: dotted;
+            }
+
+            #logo {
+                background: url(<?php bloginfo('template_url'); ?>/images/logo-2.png) no-repeat;
+                background-size: contain;
+                width: 410px;
+                height: 164px;
+                margin-left: auto;
+                margin-right: auto;
+            }
+
+        </style>
+    </head>
+    <body>
+    <div id="">
+        <div id="logo"></div>
+        <h1>Ошибка 404</h1>
+        <span>Запрашиваемая Вами страница не найдена!</span>
+        <br/>
+        <a href="javascript:history.back()">Вернуться назад</a>
+    </div>
+    </body>
+</html>
---
+++ wp-content/themes/rozengrals/images/cursors/sword/readme.txt
@@ -0,0 +1,17 @@
+﻿=== Sword Cursor Set ===
+
+By: Shelbz18
+
+Download: http://www.rw-designer.com/cursor-set/sword-1
+
+Author's decription:
+
+This is my first set hope you like. (:
+
+==========
+
+License: Released to Public Domain
+
+You are free:
+
+* To use this work for any legal purpose.
